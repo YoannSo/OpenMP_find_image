7bffc1adfe478a9c78a4191db2ff98ce2e96a876
sochaj2 <yoann.sochaj@gmail.com>
2022-04-15 11:41:00 +0200

    setup

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "lib_stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "lib_stb_image/stb_image_write.h"
+
+int main (int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Invalid arguments !\n");
+        return EXIT_FAILURE;
+    }
+
+    // Get image paths from arguments.
+    const char *inputImgPath = argv[1];
+    const char *searchImgPath = argv[2];
+
+    // ==================================== Loading input image.
+    int inputImgWidth;
+    int inputImgHeight;
+    int dummyNbChannels; // number of channels forced to 3 in stb_load.
+    unsigned char *inputImg = stbi_load(inputImgPath, &inputImgWidth, &inputImgHeight, &dummyNbChannels, 3);
+    if (inputImg == NULL)
+    {
+        printf("Cannot load image %s", inputImgPath);
+        return EXIT_FAILURE;
+    }
+    printf("Input image %s: %dx%d\n", inputImgPath, inputImgWidth, inputImgHeight);
+
+    // ====================================  Loading search image.
+    int searchImgWidth;
+    int searchImgHeight;
+    unsigned char *searchImg = stbi_load(searchImgPath, &searchImgWidth, &searchImgHeight, &dummyNbChannels, 3);
+    if (searchImg == NULL)
+    {
+        printf("Cannot load image %s", searchImgPath);
+        return EXIT_FAILURE;
+    }
+    printf("Search image %s: %dx%d\n", searchImgPath, searchImgWidth, searchImgHeight);
+
+
+
+    // ====================================  Save example: save a copy of 'inputImg'
+    unsigned char *saveExample = (unsigned char *)malloc(inputImgWidth * inputImgHeight * 3 * sizeof(unsigned char));
+    memcpy( saveExample, inputImg, inputImgWidth * inputImgHeight * 3 * sizeof(unsigned char) );
+
+    stbi_write_png("img/save_example.png", inputImgWidth, inputImgHeight, 3, saveExample, inputImgWidth * 3);
+
+    free(saveExample);
+    stbi_image_free(inputImg); 
+    stbi_image_free(searchImg); 
+
+    printf("Good bye!\n");
+
+    return EXIT_SUCCESS;
+}
